5cba19e2ae93925b58b673fd0bf73270a6f3870a
David Gunter <dog@lanl.gov>
2019-11-08 09:58:28 -0700

    Corrected mnist_loader.py

--- mnist_loader.py
+++ mnist_loader.py
@@ -1,83 +1,183 @@
+''' 
+Python Functions to load MNIST image and training data. 
+The data resides in four gzipped files stored at
+http://yann.lecun.com/exdb/mnist/. The code below will
+download these files if they do not already exist locally.
+
+   - loadImages reads in image data
+   - loadLabels reads the corresponding label data, one for each image.
+   - mnist_load packs the downloaded image and label data into a combined 
+     format to be read later by our neural network.
 '''
-mnist_loader
-------------
-A library to load the MNIST image data.
-
-The data files should be downloaded from here. The same URL also 
-contains a description.
-    http://yann.lecun.com/exdb/mnist/
-
-
-Place the files in a subdirectory named 'MNIST_data'.
-'''
-
-from mnist import MNIST
-
-#import cPickle
+import urllib.request
 import gzip
+import os
+import struct
 import numpy as np
+import matplotlib.pyplot as plt
 
-def load_data():
+def loadImages(src, n_images=None):
     '''
-    Opens the training data files (images and labels) as well as
-    the test data files and returns them in two separate objects.
+    loadImages reads image data from the MNIST image data files.
 
-    "images" is a collection of all 60,000 images and "labels" is
-    the set of matching identifiers.
+    It checks for the existence of a 'src' file and if it is not present,
+    it downloads it. 
+
+    Next, it opens the file and reads in the first 4 bytes of the data. 
+    That data is actually a 32-bit (4 byte) integer and is known as a
+    "magic number" (See description at the URL given above.) The magic 
+    number tells us whether the file contains image data or label data. 
+    If it detects anything but an image file, it prints an error
+    message and quits. (We will load label data in another function.)
+
+    It then reads the next 4 bytes, another 32-bit integer that tells us 
+    the toal number of images stored in the file. It will load all of the images
+    if n_images was not passed in, otherwise it will only read in n_images,
+    after first checking that n_images <= total images.
+
+    Inputs:
+            src: The name of a source file from which to read
+       n_images: (optional) A count of how many images to load. If not
+                 present, then all images are loaded.
+                           
     '''
-    mndata = MNIST('./MNIST_data')
-    # Leave the files in compressed gzip format and let the reader know.
-    mndata.gz = True
-    tr_imgs, tr_lbls = mndata.load_training()
-    # Load the 10,000 test image data items but split these off
-    # into a set of 5,000 validation data and 5,000 test data
-    images, labels = mndata.load_training()
-    va_imgs = images[:5000]   # Take the first 5,000
-    va_lbls = labels[:5000]
-    te_imgs = images[-5000:]  # Take the last 5,000
-    te_lbls = labels[-5000:]
-
-
-    return (tr_imgs, tr_lbls, va_imgs, va_lbls, te_imgs, te_lbls)
-
-def load_data_wrapper():
+    # This is where the data files are located
+    URLROOT = 'http://yann.lecun.com/exdb/mnist/'
+    # First check to see if the file exists and if not, download it. The line
+    # below creates a boolean value based on the existence of the file.
+    # 'True' means the file exists, 'False' means it not.
+    SRCExists = os.path.exists(src) 
+    if not SRCExists:
+       print ('Downloading ' + URLROOT + src)
+       gzfname, h = urllib.request.urlretrieve(URLROOT+src, src)
+       print ('Done.')
+    else:
+       gzfname = src
+    # Now that we have the source file, try reading it and throw 
+    # exceptions (error flags) if something bad happens.
+    try:
+        # Open a gzip'd (compressed) data file
+        with gzip.open(gzfname) as gz:
+            # Read in the first 4 bytes of the file
+            n = struct.unpack('I', gz.read(4))
+            # Read magic number. The binary data was stored as 
+            # most-significant-bytes first. Modern PCs read in data in 
+            # least-significant-bytes first. So if the magic number (in hex format)
+            # is 0x00000803 (2051 in decimal) then our Python code will actually
+            # read in the last 4-bytes first, and then the first 4 bytes. That is, 
+            # Python will load this value as 0x0803 followed by 0x0000. So this
+            # is how we check for the correct magic number value.
+            if n[0] != 0x03080000:
+                raise Exception('Invalid file: unexpected magic number.')
+            # Read number of entries from the next 4 bytes (32-bits)
+            n = struct.unpack('>I', gz.read(4))[0]
+            # Check that we haven't been asked to read in more than n images
+            if n_images == None:
+                n_images = n    # Set the number of images to read to the number present
+            elif n_images > n:
+                raise Exception('Unable to read {0} entires from data file.'.format(n_images))
+            # Now start loading images. The next 8 bytes (4 bytes at a time) 
+            # us the number of rows and the number of columns for each image
+            n_rows = struct.unpack('>I', gz.read(4))[0]
+            n_cols = struct.unpack('>I', gz.read(4))[0]
+            # Make sure we are working with the data containing 28x28 pixels
+            if n_rows != 28 or n_cols != 28:
+                raise Exception('Invalid file: expected 28 rows & cols per image.')
+            # The remainder of the data file is one continuous stream of binary data,
+            # where every 28x28=784 bytes is one separate image. We will read
+            # this data into one single array of bytes
+            res = np.frombuffer(gz.read(n_images * n_rows * n_cols), dtype = np.uint8)
+    finally:
+        # Reshape 'res' into an array of images. There will be 'n_images' images
+        # each of size n_rows * n_cols
+        return res.reshape((n_images, n_rows * n_cols))
+ 
+def loadLabels(src, n_images=None):
     '''
-    This wrapper function calls load_data() and then converts the image
-    and label data into forms that our neural network can use.
-
-    Image data is an array of images, each image being a 1d-array of 784 
-    pixel values in the range 0-254. Convert these to NumPy arrays.
-
-    The label data is vectorized to match the output nodes of the 
-    neural net. Each element of the label array becomes a 10-element 
-    1d-array representing a specific digit.
+    loadLabels is very much like loadImages.
     '''
+    URLROOT = 'http://yann.lecun.com/exdb/mnist/'
+    SRCExists = os.path.exists(src) 
+    if not SRCExists:
+        print ('Downloading ' + URLROOT + src)
+        gzfname, h = urllib.request.urlretrieve(URLROOT+src, src)
+        print ('Done.')
+    else:
+        gzfname = src
+    try:
+        with gzip.open(gzfname) as gz:
+            n = struct.unpack('I', gz.read(4))
+            # Read magic number. Check that we have a "label" file
+            if n[0] != 0x01080000:
+                raise Exception('Invalid file: unexpected magic number.')
+            # Read number of entries.
+            n = struct.unpack('>I', gz.read(4))
+            # Check that we haven't been asked to read in more than n images
+            if n_images == None:
+                n_images = n[0]    # Set the number of images to read to the number present
+            elif n_images > n[0]:
+                raise Exception('Unable to read {0} entires from label file.'.format(n_images))
+            # Read labels. The remaining file is just a stream of integers that label the
+            # corresponding images from the iamge file.
+            res = np.frombuffer(gz.read(n_images), dtype = np.uint8)
+    finally: 
+        # Return an array of these integers
+        return res.reshape((n_images, 1))
 
-    tr_images, tr_labels, va_images, va_labels, te_images, te_labels = load_data()
-
-    training_inputs = [np.reshape(x, (784, 1)) for x in tr_images]
-    training_results = [vectorized_digit(x) for x in tr_labels]
-    training_data = list(zip(training_inputs, training_results))
-
-    validation_inputs = [np.reshape(x, (784, 1)) for x in va_images]
-    #validation_results = [vectorized_digit(x) for x in va_labels]
-    #validation_data = zip(validation_inputs, validation_results)
-    validation_data = list(zip(validation_inputs, va_labels))
-
-    test_inputs = [np.reshape(x, (784, 1)) for x in te_images]
-    test_data = list(zip(test_inputs, te_labels))
-    return (training_data, validation_data, test_data)
+def mnist_load(imageSrc, labelsSrc, n_images):
+    '''
+    Inputs:
+       imageSrc: The name of a source file containing image data
+       labelSrc: The name of a file conttaining image labels
+           n_images: How many image samples to read
+    '''
+    images = loadImages(imageSrc, n_images)
+    labels = loadLabels(labelsSrc, n_images)
+    vectorized_labels = []
+    for label in labels:
+        vd = vectorized_digit(label)
+        vectorized_labels.append(vd)
+    return images, vectorized_labels
 
 def vectorized_digit(j):
     '''
-    Takes a single digit as input, j, and creates a 10-element
-    vector representing j.  The vector is all zeroes except for
-    the jth element which is set to 1.
+    Given a digt 'j', return a 10-element vector representing that digit.
+    The vector is all zeros excpet for the jth element, which is 1.
     '''
+    # Create a 10-element array of zeros.
     e = np.zeros((10, 1))
+    # Set the jth element to 1.0
     e[j] = 1.0
     return e
 
+# Code to test out the library
 if __name__ == "__main__":
-    images, labels = load_data()
-    training_data, validation_data, test_data = load_data_wrapper()
+
+    # Training image and label data
+    training_image_file = 'train-images-idx3-ubyte.gz'
+    training_label_file = 'train-labels-idx1-ubyte.gz'
+    nTrainingSamples = 60000
+
+    # Test image and label data
+    test_image_file = 't10k-images-idx3-ubyte.gz'
+    test_label_file = 't10k-labels-idx1-ubyte.gz'
+    nTestSamples = 10000
+
+    print("Loading training data.")
+    training_images, training_labels = mnist_load(training_image_file, training_label_file, nTrainingSamples)
+
+    print("Loading test data.")
+    testing_images, testing_labels = mnist_load(test_image_file, test_label_file, nTestSamples)
+
+    # visualize a random sample from the dataset
+    sample_number = 1
+    plt.imshow(training_images[sample_number].reshape(28,28), cmap="gray_r")
+    plt.axis('off')
+    plt.show()
+    print("Image Label: ", training_labels[sample_number])
+
+    sample_number = 5
+    plt.imshow(training_images[sample_number].reshape(28,28), cmap="gray_r")
+    plt.axis('off')
+    plt.show()
+    print("Image Label: ", training_labels[sample_number])
