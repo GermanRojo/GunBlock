8df497bff434c8de252beb81ca92b573f0ff97c3
David Gunter <dogunter@gmail.com>
2020-01-30 07:59:45 -0700

    Added README.data_setup in src

--- src/data_setup.py
+++ src/data_setup.py
@@ -1,3 +1,10 @@
+# This script will process are VIA annotation file and split up the images
+# into a training set and a validation set for use with the
+# Mask_RCNN neural network model
+#
+# Run it from this directory as
+#   python3 data_setup.py
+
 from random import random
 import os
 from pathlib import Path
@@ -5,10 +12,10 @@ from glob import glob
 import json
 
 # Path to our images
-image_paths = glob("../images-edwardo/*")
+image_paths = glob("../images/*")
 
 # Path to JSON annotation file from the VIA tool
-annotation_file = '../GunBlock-edwardo-200.json'
+annotation_file = '../GunBlock-polygons-358.json'
 
 # Clean up the annotations a little:
 # - remove all the VIA app settings and attribute definition bits from 
@@ -19,8 +26,8 @@ for k,v in annotations['_via_img_metadata'].items():
     cleaned_annotations[v['filename']] = v
 
 # Create train and validation directories
-Path("procdata/val").mkdir(parents=True, exist_ok=True)
-Path("procdata/train").mkdir(parents=True, exist_ok=True)
+Path("gundata/val").mkdir(parents=True, exist_ok=True)
+Path("gundata/train").mkdir(parents=True, exist_ok=True)
 
 train_annotations = {}
 valid_annotations = {}
@@ -28,18 +35,18 @@ valid_annotations = {}
 for img in image_paths:
     # Image goes to Validation folder
     if random()<0.2:
-        os.system("cp "+ img + " procdata/val/")
+        os.system("cp "+ img + " gundata/val/")
         img = img.split("/")[-1]
         valid_annotations[img] = cleaned_annotations[img]
     else:
-        os.system("cp "+ img + " procdata/train/")
+        os.system("cp "+ img + " gundata/train/")
         img = img.split("/")[-1]
         train_annotations[img] = cleaned_annotations[img]
 
 # Split the annotation data into the appropriate folders,
 # i.e. keep the training annotation data in the train folder
 # and the validation annotation data in the val folder
-with open('procdata/val/region_data.json', 'w') as fp:
+with open('gundata/val/region_data.json', 'w') as fp:
     json.dump(valid_annotations, fp)
-with open('procdata/train/region_data.json', 'w') as fp:
+with open('gundata/train/region_data.json', 'w') as fp:
     json.dump(train_annotations, fp)
